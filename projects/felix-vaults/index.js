97061e72a3aa3869e13c2f847fc82f8e0cd8fca5
RED TIGER <RedTigerAuditor@gmail.com>
2026-09-16 21:29:48 +0000

    expand Felix Vaults TVL adapter

--- projects/felix-vaults/index.js
+++ projects/felix-vaults/index.js
@@ -1,12 +1,21 @@
-const { getMorphoVaultTvl } = require("../helper/morpoho");
+const { getCuratorTvl, getMorphoVaults } = require("../helper/curators");
 
 const hyperliquidConfig = {
   governor: "0x4A827418D632C415E19825fd011283A4ba020B3A",
 }
 
+async function tvl(api) {
+  // Shared discovery now covers both the V1 and V2 HyperEVM factories.
+  const allVaults = await getMorphoVaults(api, [], { getAllVaults: true })
+  const owners = await api.multiCall({ abi: 'address:owner', calls: allVaults })
+  const vaults = allVaults.filter((_, i) => owners[i].toLowerCase() === hyperliquidConfig.governor.toLowerCase())
+
+  return getCuratorTvl(api, { morpho: vaults })
+}
+
 module.exports = {
   doublecounted: true,
   hyperliquid: {
-    tvl: getMorphoVaultTvl(hyperliquidConfig.governor)
+    tvl,
   },
 }
